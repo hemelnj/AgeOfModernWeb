0fb9eeea7f82847ca3e34b477df3dd19bf96ed10
hemelnj <hemel_nj@live.com>
2015-05-19 00:44:02 +0600

    Add Admin Area

---
+++ KnowledgeArsenal/KnowledgeArsenal/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KnowledgeArsenal.Areas.Admin.Controllers
+{
+    public class UsersController : Controller
+    {
+        //
+        // GET: /Admin/Users/
+
+        public ActionResult Index()
+        {
+            return Content("From User Control Index Action");
+        }
+
+    }
+}
