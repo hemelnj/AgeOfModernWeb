2109e7c9017022cbb07293c9498500f3e4b6e22f
hemelnj <hemel_nj@live.com>
2015-05-21 00:31:33 +0600

    Added Basic Login Form

--- KnowledgeArsenal/KnowledgeArsenal/Controllers/AuthController.cs
+++ KnowledgeArsenal/KnowledgeArsenal/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using KnowledgeArsenal.ViewModels;
 
 namespace KnowledgeArsenal.Controllers
 {
@@ -10,11 +11,16 @@ namespace KnowledgeArsenal.Controllers
     {
         //
         // GET: /Auth/
-
+        [HttpGet]
         public ActionResult Login()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            return Content("User Name is"+form.UserName);
+        }
     }
 }
